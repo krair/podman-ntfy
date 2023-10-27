799e0065f896a5a6b826781d52eb2fd28c568140
krair <info@rair.dev>
2023-10-27 15:57:43 +0200

    Remove 'insecure' flag from curl command

--- podman-ntfy.sh
+++ podman-ntfy.sh
@@ -9,8 +9,8 @@ updates=$(podman auto-update --dry-run --format json | \
           jq -r 'map(select(.Updated =="pending").ContainerName) | join(", ")')
 
 if ! [[ -z "$updates" ]]; then
-  curl --insecure -H prio:3 -H 'Title: Container Updates available!' -d "${updates}" ${ntfy_url}/${ntfy_topic}
+  curl -H prio:3 -H 'Title: Container Updates available!' -d "${updates}" ${ntfy_url}/${ntfy_topic}
 else
-  curl --insecure -H prio:2 -H 'Title: No Container Updates' -d "Hooray!" ${ntfy_url}/${ntfy_topic}
+  curl -H prio:2 -H 'Title: No Container Updates' -d "Hooray!" ${ntfy_url}/${ntfy_topic}
 fi
 
